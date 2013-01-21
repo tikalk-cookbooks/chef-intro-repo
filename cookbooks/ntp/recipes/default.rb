9f2fc3352f4f168e56b25d176319b0b10c498217
hagzag <hagzag@tikalk.com>
2013-01-21 22:16:47 +0200

    Update recipe to support synamic service name resolution ...

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -12,15 +12,17 @@ package "ntp" do				# "ntp" applies to both ubuntu and centos
     action [:install]				# install package
 end
  
+service node[:ntp][:service] do
+    service_name node[:ntp][:service]         
+    action [:enable,:start,:restart]                   
+end
+
 template "/etc/ntp.conf" do			# create / edit the /etc/ntp.conf file
     source "ntp.conf.erb"			# defaults to templates/files/...
     owner "root" 				# set file owner
     group "root"				# set file group
     mode 0644					# set file mode
-    notifies :restart, service_name node[:ntp][:service]		# restart service as post config action
+    notifies :restart, resources(:service => node[:ntp][:service])#, :delayed
 end
- 
-service "ntpd" do
-    service_name node[:ntp][:service]		# see 
-    action [:enable,:start]			# use chkconfig on centos | update-rc.d on ubutnu
-end
+
+
